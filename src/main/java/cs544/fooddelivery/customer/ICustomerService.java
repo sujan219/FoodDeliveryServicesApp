d7eb6f4c7bb0584eec4b5b56456af77a887ff416
prasannashrestha.64@gmail.com <prasannashrestha.64@gmail.com>
2016-09-24 12:00:42 -0500

    Customer dashboard added

--- src/main/java/cs544/fooddelivery/customer/ICustomerService.java
+++ src/main/java/cs544/fooddelivery/customer/ICustomerService.java
@@ -1,5 +1,14 @@
 package cs544.fooddelivery.customer;
 
+import java.util.List;
+
+import cs544.fooddelivery.domain.Consumer;
+import cs544.fooddelivery.domain.Order;
+
 public interface ICustomerService {
+	
+	Consumer update(Consumer customer);
+	List<Order> orders();
+	List<Order> orderHistory();
 
 }
